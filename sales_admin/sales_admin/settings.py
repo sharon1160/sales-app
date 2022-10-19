554de1b87525de4e72699ed983bce1b6be0739b3
Sharon CR <schullunquia@unsa.edu.pe>
2022-10-18 21:41:23 -0500

    add page number pagination

--- sales_admin/sales_admin/settings.py
+++ sales_admin/sales_admin/settings.py
@@ -50,7 +50,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', 'rest_framework.authentication.SessionAuthentication'),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 4
 }
 
 MIDDLEWARE = [
