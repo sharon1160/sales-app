c7101d8f2e8fb7f2fbf86ddb1e84250740bb551e
Sharon CR <schullunquia@unsa.edu.pe>
2022-10-20 18:12:46 -0500

    change path

--- sales_admin/applications/sales/admin.py
+++ sales_admin/applications/sales/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from applications.sales.models import Currency
+from applications.warehouse.models import Currency
 
 # Agregamos al modelo Currency al django admin
 admin.site.register(Currency)
