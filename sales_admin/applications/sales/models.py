af4cc0a6cfd8bbd9eb506f98f423722f8b7b83b5
Sharon CR <schullunquia@unsa.edu.pe>
2022-10-20 18:15:32 -0500

    add models sales

--- sales_admin/applications/sales/models.py
+++ sales_admin/applications/sales/models.py
@@ -1,36 +1,129 @@
+"""
+En el archivo models.py agregamos los modelos que utilizará nuestra aplicación.
+Los modelos de esta aplicación pueden ser utilizados desde otras aplicaciones.
+"""
+# Importamos la clase MinLengthValidator para agregar validacion
+# Link: https://docs.djangoproject.com/en/4.1/ref/validators/#module-django.core.validators
+from django.core.validators import MinLengthValidator
+
+# Importamos las clases MinValueValidator, MaxValueValidator para agregar validaciones
+# al atributo "quantity"
+# Link: https://docs.djangoproject.com/en/4.1/ref/validators/#module-django.core.validators
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Importamos la clase models del módulo django.db
+# Link: https://docs.djangoproject.com/en/4.1/topics/db/models/
 from django.db import models
 
-# Create your models here.
+# Importamos la clase Product de la aplicación Warehouse
+from applications.warehouse.models import Product
 
-class Currency(models.Model):
+
+class Customer(models.Model):
     """
-    Clase Moneda.
-    Ejemplo 1: Moneda Sol
-    Código: PEN
-    Simbolo: S/.
-    Nombre: Sol Peruano
+    Clase Cliente
 
-    Ejemplo 2: Moneda Dólares Americanos.
-    Código: USD
-    Simbolo: $.
-    Nombre: Dólares Americanos
     """
-
     id = models.AutoField(primary_key=True)
 
-    code = models.CharField(max_length=3,unique=True, verbose_name="Código")
+    code = models.CharField(max_length=5, unique=True, verbose_name="Código")
 
-    symbol = models.CharField(max_length=4, verbose_name="Simbolo")
+    # razon social
+    business_name = models.CharField(
+        max_length=60, blank=False, verbose_name="Razón Social")
 
-    name = models.CharField(max_length=20, verbose_name="Nombre")
+    # categoria
+    category = models.CharField(max_length=50, verbose_name="Categoría")
 
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    # RUC del cliente (único, y de 11 dígitos)
+    ruc = models.CharField(unique=True, max_length=11, validators=[
+        MinLengthValidator(11)], verbose_name="RUC")
 
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha de Moficación")
-
-    def __str__(self):
-        return f"{self.symbol} {self.code}"
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de Creación")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de Modificación")
 
     class Meta:
-        db_table = "currency"
-        verbose_name = "Moneda"
+        db_table = "customer"
+        verbose_name = "Cliente"
+
+
+class Order(models.Model):
+    """
+    Clase Pedido
+
+    """
+    id = models.AutoField(primary_key=True)
+
+    # numero de pedido
+    number = models.CharField(unique=True, max_length=5, validators=[
+                              MinLengthValidator(5)], verbose_name="Número de Pedido")
+
+    # fecha de pedido
+    date = models.DateTimeField(verbose_name="Fecha de Pedido")
+
+    # direccion de entrega
+    delivery_address = models.CharField(
+        max_length=150, verbose_name="Dirección de Entrega")
+
+    # fecha de entrega
+    delivery_date = models.DateTimeField(verbose_name="Fecha de Entrega")
+
+    # foreign_key: cliente
+    customer_id = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, db_column="customer_id", verbose_name="Cliente")
+
+    subtotal = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, verbose_name="Subtotal")
+
+    igv = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, verbose_name="IGV")
+
+    total = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, verbose_name="Total")
+
+    # descuento total
+    discount_total = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, verbose_name="Descuento Total")
+
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de Creación")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de Modificación")
+
+    class Meta:
+        db_table = "order"
+        verbose_name = "Pedido"
+
+
+class OrderItem(models.Model):
+    """
+    Clase Item del pedido
+
+    """
+    id = models.AutoField(primary_key=True)
+
+    # foreign_key: Pedido
+    order_id = models.ForeignKey(
+        Order, on_delete=models.CASCADE, db_column="order_id", verbose_name="Pedido")
+
+    # foreign_key: Producto
+    product_id = models.ForeignKey(
+        Product, on_delete=models.CASCADE, db_column="product_id", verbose_name="Producto")
+
+    # cantidad de productos
+    quantity = models.PositiveSmallIntegerField(default=0, validators=[
+        MinValueValidator(0), MaxValueValidator(200)], verbose_name="Cantidad")
+
+    total = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, verbose_name="Total")
+
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Fecha de Creación")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de Modificación")
+
+    class Meta:
+        db_table = "order_item"
+        verbose_name = "Item Pedido"
